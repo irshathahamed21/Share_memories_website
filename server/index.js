dfb3f50ee2a6c6ec74ebbf061f691a7c080fd6b2
irshathahamed21 <21irshathkhan@gmail.com>
2022-06-17 15:37:08 +0530

    mongodb url error fixed

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const CONNECTION_URL = process.env.MONGO_URL
 const PORT = process.env.PORT || 5000
 
 
-await mongoose.connect(CONNECTION_URL)
+await mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => 
         console.log(`Server running on port: ${PORT}`) 
     ) )
