19447426741bdaceb72de186d2f1e843c2d5d4b7
irshathahamed21 <21irshathkhan@gmail.com>
2022-06-17 15:33:56 +0530

    fixed bugs

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,21 @@
 import React from "react"
 import ReactDom from "react-dom"
 
+
 import App from "./App.js"
 
-ReactDom.render(<App/>, document.getElementById("root"))
+import {createStore, applyMiddleware,compose} from "redux"
+import {Provider} from "react-redux"
+
+import thunk from "redux-thunk"
+import { reducers } from "./reducers/index.js"
+import "./index.css"
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+ReactDom.render(
+<Provider store = {store}>
+<App/>
+</Provider>
+, document.getElementById("root"))
+
