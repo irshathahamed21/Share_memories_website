491e8014907ff1096e05b9f4d29c959dec4b5b40
irshathahamed21 <21irshathkhan@gmail.com>
2022-06-17 15:32:54 +0530

    added hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Container, AppBar,Typography, Grow, Grid} from "@material-ui/core"
 import Posts from "./components/Posts/Posts"
 import Form from "./components/Form/Form"
 import useStyles from './styles';
+import {useDispatch} from "react-redux"
+import {getPosts} from "./actions/posts"
 
 function App() {
+  const [currentId, setCurrentId] = useState()
   const classes = useStyles()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+        dispatch(getPosts())
+  },[currentId, dispatch])
   
   return (
     <Container maxWidth = "lg">
@@ -15,12 +23,12 @@ function App() {
       </AppBar>
       <Grow in>
         <Container>
-        <Grid container justify= "space-between" alignItems= "stretch" spacing = {3}>
+        <Grid container justifyContent= "space-between" alignItems= "stretch" spacing = {3}>
           <Grid item xs = {12} sm = {7}>
-            <Posts/>
+            <Posts setCurrentId = {setCurrentId} />
           </Grid>
           <Grid item xs = {12} sm = {4}>
-            <Form/>
+            <Form currentId = {currentId} setCurrentId = {setCurrentId}/>
           </Grid> 
         </Grid>
         </Container>
